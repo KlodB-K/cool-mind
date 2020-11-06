ff8757afe6cad50c5e3bd8d93c930e6ab8c12bea
KlodB-K <klaudia.baranska8@gmail.com>
2020-11-06 13:54:32 +0100

    Moving code from hamburger to index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,4 +6,17 @@ if (process.env.NODE_ENV === "development") {
 
 console.log("Hello")
 
-
+window.onload = init;
+function init() {
+  var button = document.getElementById("hamburger");
+  button.onclick = handleButtonClick;
+}
+showMenu();
+function handleButtonClick() {
+  var menu = document.getElementById("menu");
+  if (menu.style.display == "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
