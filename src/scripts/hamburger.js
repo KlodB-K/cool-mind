344458ce004314f35c52f8c7d2f463f0b9f2ad53
KlodB-K <klaudia.baranska8@gmail.com>
2020-11-06 12:59:42 +0100

    Display menu (WIP)

---
+++ src/scripts/hamburger.js
@@ -0,0 +1,16 @@
+window.onload = init;
+
+function init() {
+var button = document.getElementById("hamburger”);
+button.onclick = handleButtonClick;
+
+showMenu();
+
+function handleButtonClick()
+var menu = document.getElementById("menu");
+if (menu.style.display == "block") {
+  menu.style.display = "none";
+} else {
+  menu.style.display = "block";
+}
+
