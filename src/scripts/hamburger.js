72403b47e6ef8b42f77713e02378129c0188887e
KlodB-K <klaudia.baranska8@gmail.com>
2020-11-06 13:44:19 +0100

    Fix to hamburger code

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -1,16 +1,14 @@
 window.onload = init;
-
 function init() {
-var button = document.getElementById("hamburger”);
-button.onclick = handleButtonClick;
-
-showMenu();
-
-function handleButtonClick()
-var menu = document.getElementById("menu");
-if (menu.style.display == "block") {
-  menu.style.display = "none";
-} else {
-  menu.style.display = "block";
+  var button = document.getElementById("hamburger");
+  button.onclick = handleButtonClick;
 }
-
+showMenu();
+function handleButtonClick() {
+  var menu = document.getElementById("menu");
+  if (menu.style.display == "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
